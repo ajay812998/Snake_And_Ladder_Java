a43b7d3e2460ae60dce184efc427596e583a0fed
ajay812998 <ajay812998@gmail.com>
2021-01-12 15:56:35 +0530

    Added : Player Rolls the Dice

--- src/com/bridgelabz/Game.java
+++ src/com/bridgelabz/Game.java
@@ -1,8 +1,15 @@
 package com.bridgelabz;
 
 public class Game {
-	int position=0;
-	public static void main(String[] args) {
-		System.out.println("only one user can play");
-	}
+	 int position=0;
+		public static void main(String[] args) {
+			System.out.println("only one user can play");
+			rolls_dice();
+		}
+	    
+		public static void rolls_dice() {
+	    	int dice=(int)(Math.floor(Math.random()*10)%6+1);
+	    	System.out.println("the dice is "+dice);
+	   }
 }
+	
