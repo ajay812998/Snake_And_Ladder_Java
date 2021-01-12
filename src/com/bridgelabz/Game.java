62399deb0e1721fc21488aae67df94007af7479d
ajay812998 <ajay812998@gmail.com>
2021-01-12 16:05:16 +0530

    Added : Checks For a Option

--- src/com/bridgelabz/Game.java
+++ src/com/bridgelabz/Game.java
@@ -1,15 +1,30 @@
 package com.bridgelabz;
 
 public class Game {
-	 int position=0;
-		public static void main(String[] args) {
-			System.out.println("only one user can play");
-			rolls_dice();
-		}
-	    
-		public static void rolls_dice() {
-	    	int dice=(int)(Math.floor(Math.random()*10)%6+1);
-	    	System.out.println("the dice is "+dice);
-	   }
-}
+	public static void main(String[] args) {
+		System.out.println("only one user can play");
+		option();
+	}
 	
+    public static int rolls_dice() {
+    	int dice=(int)(Math.floor(Math.random()*10)%6+1);
+    	return dice;
+    }
+    
+    public static void option() {
+    	int position=0;
+    	int luck=(int)(Math.floor(Math.random()*10)%3+1);
+    	int dice=rolls_dice();
+    	System.out.println("the dice is "+dice);
+    	switch (luck) {
+    	case 1:System.out.println("the position is "+position);
+    			break;
+    	case 2:	position=position+dice;
+    			System.out.println("the position is "+position);
+    			break;
+    	case 3:position=position-dice;
+    			System.out.println("the postition is "+position);
+    			break;
+    	}
+    }
+}
