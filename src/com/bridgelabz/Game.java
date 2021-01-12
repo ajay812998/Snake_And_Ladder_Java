07c8a468d8fd3b029ef1863a98dd651d998431a5
ajay812998 <ajay812998@gmail.com>
2021-01-12 15:51:10 +0530

    Added : Single Player at Start Position 0

--- src/com/bridgelabz/Game.java
+++ src/com/bridgelabz/Game.java
@@ -1,8 +1,8 @@
 package com.bridgelabz;
 
 public class Game {
+	int position=0;
 	public static void main(String[] args) {
-		System.out.println("Welcome to Snake and Ladder Game");
+		System.out.println("only one user can play");
 	}
 }
-    
